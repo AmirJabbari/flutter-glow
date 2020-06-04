9c857e8b036bd0a24976b90427e9f06a020aa53a
Payam Zahedi <pzahedi1995@gmail.com>
2020-06-04 19:02:04 +0430

    GlowThemeData class created

---
+++ lib/src/theme/theme.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+
+@immutable
+class GlowThemeData {
+  const GlowThemeData({
+    this.glowColor,
+    this.offset,
+    this.spreadRadius,
+    this.blurRadius,
+  });
+
+  factory GlowThemeData.fromBoxShadow({BoxShadow boxShadow}) => GlowThemeData(
+        glowColor: boxShadow.color,
+        offset: boxShadow.offset,
+        spreadRadius: boxShadow.spreadRadius,
+        blurRadius: boxShadow.blurRadius,
+      );
+
+  final Color glowColor;
+  final Offset offset;
+  final double spreadRadius;
+  final double blurRadius;
+
+  GlowThemeData copyWith({
+    Color glowColor,
+    Offset offset,
+    double spreadRadius,
+    double blurRadius,
+  }) =>
+      GlowThemeData(
+        glowColor: glowColor ?? glowColor,
+        offset: offset ?? offset,
+        spreadRadius: spreadRadius ?? spreadRadius,
+        blurRadius: blurRadius ?? blurRadius,
+      );
+
+  GlowThemeData copyFrom({
+    GlowThemeData other,
+  }) =>
+      GlowThemeData(
+        glowColor: other.glowColor ?? this.glowColor,
+        offset: other.offset ?? this.offset,
+        spreadRadius: other.spreadRadius ?? this.spreadRadius,
+        blurRadius: other.blurRadius ?? this.blurRadius,
+      );
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+          other is GlowThemeData &&
+              runtimeType == other.runtimeType &&
+              glowColor == other.glowColor &&
+              offset == other.offset &&
+              spreadRadius == other.spreadRadius &&
+              blurRadius == other.blurRadius;
+
+  @override
+  int get hashCode =>
+      glowColor.hashCode ^
+      offset.hashCode ^
+      spreadRadius.hashCode ^
+      blurRadius.hashCode;
+
+  @override
+  String toString() {
+    return 'GlowThemeData{glowColor: $glowColor, offset: $offset, spreadRadius: $spreadRadius, blurRadius: $blurRadius}';
+  }
+
+}
