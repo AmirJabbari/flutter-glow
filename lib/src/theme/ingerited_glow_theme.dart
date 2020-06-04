1127012d676144873d75ddba00a0b542db920754
Payam Zahedi <pzahedi1995@gmail.com>
2020-06-04 20:17:42 +0430

    GlowThemeInherited class created

---
+++ lib/src/theme/ingerited_glow_theme.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:flutterglow/src/theme/theme.dart';
+import 'package:flutterglow/src/theme/theme_handler.dart';
+import 'package:flutterglow/src/theme/glow_theme_type.dart';
+
+class GlowThemeInherited extends InheritedWidget {
+  const GlowThemeInherited({
+    Key key,
+    @required Widget child,
+    @required this.value,
+    @required this.onChange,
+  })  : assert(child != null),
+        super(key: key, child: child);
+
+  final GlowThemeHandler value;
+
+  final ValueChanged<GlowThemeHandler> onChange;
+
+  @override
+  bool updateShouldNotify(GlowThemeInherited oldWidget) => value != oldWidget.value;
+
+  GlowThemeData get current => value.current;
+
+  bool get isUsingDark => value.useDark;
+
+  GlowThemeType get themeType => value.themeType;
+
+  set themeType(GlowThemeType themeType) => onChange(value.copyWith(themeType: themeType));
+
+  set updateCurrentTheme(GlowThemeData themeData) => onChange(
+        value.copyWith(
+          theme: isUsingDark ? null : themeData,
+          darkTheme: isUsingDark ? themeData : null,
+        ),
+      );
+}
