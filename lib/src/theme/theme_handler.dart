c866d40a8114f11ddab8e343a4632abede95354c
Payam Zahedi <pzahedi1995@gmail.com>
2020-06-04 20:17:22 +0430

    GlowThemeHandler class created

---
+++ lib/src/theme/theme_handler.dart
@@ -0,0 +1,56 @@
+import 'dart:ui';
+
+import 'package:flutterglow/src/theme/theme.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutterglow/src/theme/glow_theme_type.dart';
+
+class GlowThemeHandler {
+  GlowThemeHandler({
+    @required this.theme,
+    this.darkTheme,
+    this.themeType = GlowThemeType.system,
+  });
+
+  final GlowThemeData theme;
+  final GlowThemeData darkTheme;
+  final GlowThemeType themeType;
+
+  bool get useDark =>
+      darkTheme != null &&
+      ( //forced to use DARK by user
+          themeType == GlowThemeType.dark ||
+              //The setting indicating the current brightness mode of the host platform.
+              // If the platform has no preference, platformBrightness defaults to Brightness.light.
+              window.platformBrightness == Brightness.dark);
+
+  GlowThemeData get current {
+    if (useDark) {
+      return darkTheme;
+    } else {
+      return theme;
+    }
+  }
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is GlowThemeHandler &&
+          runtimeType == other.runtimeType &&
+          theme == other.theme &&
+          darkTheme == other.darkTheme &&
+          themeType == other.themeType;
+
+  @override
+  int get hashCode => theme.hashCode ^ darkTheme.hashCode ^ themeType.hashCode;
+
+  GlowThemeHandler copyWith({
+    GlowThemeData theme,
+    GlowThemeData darkTheme,
+    GlowThemeType themeType,
+  }) =>
+      GlowThemeHandler(
+        theme: theme ?? this.theme,
+        darkTheme: darkTheme ?? this.darkTheme,
+        themeType: themeType ?? this.themeType,
+      );
+}
