e73e77d34afb4a2ffe39e38c7862fadb9a6d2abe
Payam Zahedi <pzahedi1995@gmail.com>
2020-06-05 10:43:42 +0430

    GlowContainer created

--- lib/flutter_glow.dart
+++ lib/flutter_glow.dart
@@ -1,3 +1,8 @@
 library flutter_glow;
 
+/// export themes related classes
+export 'package:flutter_glow/src/theme/glow_theme.dart';
+
+/// export widget related classes
 export 'package:flutter_glow/src/widget/glow_button.dart';
+export 'package:flutter_glow/src/widget/glow_container.dart';
--- lib/src/widget/glow_container.dart
+++ lib/src/widget/glow_container.dart
@@ -1,8 +1,68 @@
 import 'package:flutter/material.dart';
 
+import '../../flutter_glow.dart';
+
 class GlowContainer extends StatelessWidget {
+  const GlowContainer({
+    Key key,
+    this.height,
+    this.width,
+    this.padding,
+    this.margin,
+    this.color,
+    this.alignment,
+    this.borderRadius,
+    this.border,
+    this.shape,
+    this.glowColor,
+    this.offset,
+    this.spreadRadius,
+    this.blurRadius,
+    this.child,
+  }) : super(key: key);
+
+  final double height;
+  final double width;
+  final Color color;
+  final BorderRadiusGeometry borderRadius;
+  final AlignmentGeometry alignment;
+  final BoxBorder border;
+  final EdgeInsetsGeometry padding;
+  final EdgeInsetsGeometry margin;
+  final BoxShape shape;
+  final Widget child;
+
+  //glow properties
+  final Color glowColor;
+  final Offset offset;
+  final double spreadRadius;
+  final double blurRadius;
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    final glowTheme = GlowTheme.of(context);
+    return AnimatedContainer(
+      height: height,
+      width: width,
+      padding: padding,
+      margin: margin,
+      alignment: alignment,
+      duration: const Duration(milliseconds: 300),
+      decoration: BoxDecoration(
+        color: color,
+        borderRadius: borderRadius ?? BorderRadius.circular(0),
+        boxShadow: [
+          BoxShadow(
+            color: glowColor ?? glowTheme?.glowColor ?? kDefaultGlowTheme.glowColor,
+            offset: offset ?? glowTheme?.offset ?? kDefaultGlowTheme.offset,
+            blurRadius: blurRadius ?? glowTheme?.blurRadius ?? kDefaultGlowTheme.blurRadius,
+            spreadRadius: spreadRadius ?? glowTheme?.spreadRadius ?? kDefaultGlowTheme.spreadRadius,
+          ),
+        ],
+        border: border,
+        shape: shape ?? BoxShape.rectangle,
+      ),
+      child: child,
+    );
   }
 }
