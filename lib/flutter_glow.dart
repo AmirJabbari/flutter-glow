f11183d42cac4c48bd2e3908001dca0aeac97ef5
Payam Zahedi <pzahedi1995@gmail.com>
2020-06-05 10:20:09 +0430

    flutter_glow.dart updated

--- lib/flutter_glow.dart
+++ lib/flutter_glow.dart
@@ -1,7 +1,3 @@
 library flutter_glow;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
+export 'package:flutter_glow/src/widget/glow_button.dart';
