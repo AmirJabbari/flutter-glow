5537c73fea670640afdb1a2c53770e9870d1e444
Rafo <66031839+rafaelovandov@users.noreply.github.com>
2022-06-20 15:15:36 +0300

    Update glow_text.dart

--- lib/src/widget/glow_text.dart
+++ lib/src/widget/glow_text.dart
@@ -2,7 +2,7 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
-import '../../flutter_glow.dart';
+import '../../flutter_glow.dart'; //fix this import
 
 class GlowText extends Text {
   const GlowText(
